b28929de63475ec58dac6e45a563c6e17f89d9d8
Anass Azeroual <azeroual.anass2015@gmail.com>
2024-05-12 18:35:02 +0100

    fix bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: '**', pathMatch: 'full', redirectTo: '/welcome' },
   { path: 'login', component:LoginComponent },
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
   { path: 'monitor', loadChildren: () => import('./pages/monitor/monitor.module').then(m => m.MonitorModule) },
@@ -14,7 +13,7 @@ const routes: Routes = [
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{enableTracing:true,useHash:true})],
+  imports: [RouterModule.forRoot(routes,{enableTracing:false,useHash:false})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
