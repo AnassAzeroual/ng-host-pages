7f997bf95d8bcb000675eeb4d27e4e6cf2d4af78
Anass Azeroual <azeroual.anass2015@gmail.com>
2024-05-12 19:52:41 +0100

    try fix reload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NzConfigService } from 'ng-zorro-antd/core/config';
 import { ThemeService } from './theme/theme.service';
 
@@ -7,9 +7,18 @@ import { ThemeService } from './theme/theme.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   isCollapsed = false;
   constructor(private theme: ThemeService,private nzConfigService: NzConfigService) {}
+
+  ngOnInit() {
+    window.addEventListener('beforeunload', (event) => {
+      event.returnValue = 'lol'; // Optional: Display a confirmation message (see notes below)
+      // Redirect user to your desired URL:
+      window.location.href = 'https://anassazeroual.github.io/ng-host-pages/';
+    });
+  }
+
   toggle() {
     const active = this.theme.getActiveTheme() ;
     if (active.name === 'light') {
@@ -18,4 +27,9 @@ export class AppComponent {
       this.theme.setTheme('light');
     }
   }
+
+  ngOnDestroy() {
+    window.removeEventListener('beforeunload', (event) => { /* ... */ });
+  }
+  
 }
