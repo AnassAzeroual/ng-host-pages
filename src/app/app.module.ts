b1e47c78b9333b3b984df144fcd3b22c738976ec
Anass Azeroual <azeroual.anass2015@gmail.com>
2024-05-12 18:02:43 +0100

    Service Worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -21,6 +21,7 @@ import { NzFormModule } from 'ng-zorro-antd/form';
 import { ThemeModule } from './theme/theme.module';
 import { lightTheme } from './theme/light-theme';
 import { darkTheme } from './theme/dark-theme';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 registerLocaleData(en);
 const ngZorroConfig: NzConfig = {
@@ -50,6 +51,12 @@ const ngZorroConfig: NzConfig = {
     ThemeModule.forRoot({
       themes: [lightTheme, darkTheme],
       active: 'light'
+    }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
     })
   ],
   providers: [
