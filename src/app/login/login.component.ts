141f7e29b563c228c4fd65b6b48d0d4870f70c92
Anass Azeroual <azeroual.anass2015@gmail.com>
2024-05-19 01:34:36 +0100

    useless

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
 import { NzEmptyModule } from 'ng-zorro-antd/empty';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  standalone:true,
-  imports:[NzEmptyModule],
+  standalone: true,
+  imports: [NzEmptyModule],
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-    constructor() {}
+  constructor() { }
 }
