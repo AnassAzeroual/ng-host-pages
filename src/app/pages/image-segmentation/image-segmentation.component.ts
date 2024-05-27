6d96ac6a8736d6fcc8b2529599562c18af41e578
Anass Azeroual <azeroual.anass2015@gmail.com>
2024-05-27 20:24:20 +0100

    "Updated rectangle coordinates and grid settings in ImageSegmentationComponent"

--- src/app/pages/image-segmentation/image-segmentation.component.ts
+++ src/app/pages/image-segmentation/image-segmentation.component.ts
@@ -12,8 +12,8 @@ export class ImageSegmentationComponent {
   private ctx!: CanvasRenderingContext2D;
 
   private rectangles = [
-    { 'x': 343, 'y': 535, 'width': 200, 'height': 100, 'area': 0.0 },
-    { 'x': 436, 'y': 339, 'width': 360, 'height': 240, 'area': 212.5 },
+    { 'x': 43, 'y': 35, 'width': 200, 'height': 100, 'area': 0.0 },
+    { 'x': 636, 'y': 39, 'width': 360, 'height': 240, 'area': 212.5 },
     { x: 200, y: 150, width: 80, height: 120 }
   ];
 
@@ -31,20 +31,16 @@ export class ImageSegmentationComponent {
       this.canvas.nativeElement.height = img.height;
       this.ctx.drawImage(img, 0, 0);
       this.drawGridOverlay();
-      this.drawRectangles();
     };
   }
 
   private drawGridOverlay() {
-    const gridSize = 13; // Size of the grid cells
-    this.ctx.strokeStyle = 'rgba(0, 0, 0, 0.1)'; // Light gray grid lines
-    this.ctx.lineWidth = 1.5;
-
+    const gridSize = 5;// a variable that define the size of a 2D grid
     for (let x = 0; x < this.canvas.nativeElement.width; x += gridSize) {
       for (let y = 0; y < this.canvas.nativeElement.height; y += gridSize) {
         // Check if the current grid cell overlaps with any rectangle
         if (!this.isInsideAnyRectangle(x, y, gridSize, gridSize)) {
-          this.ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
+          this.ctx.fillStyle = 'rgba(0, 0, 0, 0.6)';
           this.ctx.fillRect(x, y, gridSize, gridSize);
         }
       }
@@ -56,13 +52,5 @@ export class ImageSegmentationComponent {
       x < rect.x + rect.width && x + width > rect.x &&
       y < rect.y + rect.height && y + height > rect.y
     );
-  }
-
-  private drawRectangles() {
-    this.ctx.strokeStyle = 'pink';
-    this.ctx.lineWidth = 2;
-    this.rectangles.forEach(rect => {
-      this.ctx.strokeRect(rect.x, rect.y, rect.width, rect.height);
-    });
   }
 }
